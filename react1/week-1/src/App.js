5259c501d18987ab6e3415e7517de49f74536418
jaeuk Lee <lskkhj1005@naver.com>
2023-02-03 19:54:49 +0900

    react setting

---
+++ react1/week-1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+// 이 파일이 우리의 PG
+function App() {
+  return (
+    <div className="App">
+      <p>Hello React !!</p>
+    </div>
+  );
+}
+
+export default App;
