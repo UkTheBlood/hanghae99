92e1064a149227b3aafd760039a1bb591932cd07
jaeuk Lee <lskkhj1005@naver.com>
2023-02-04 20:28:31 +0900

    state

--- react1/week-1/src/App.js
+++ react1/week-1/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import Child from './Child'
+import React, { useState } from 'react'
 
 function App() {
-  const name = 'test'
-  return <Child 
-            // age={21} 
-            name={name}>이름
-          </Child>
+  // id와 password state 만들기
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onIdChangeHandler = (event) => {
+    setId(event.target.value)
+  }
+
+  const onPasswordChangeHandler = (event) => {
+    setPassword(event.target.value)
+  }
+
+  return (
+    <div>
+      아이디: <input value = {id} onChange ={onIdChangeHandler}/>
+
+      <br />
+
+      비밀번호: <input value = {password} type = "password" onChange={onPasswordChangeHandler}/>
+
+      <br />
+
+      <button onClick={function () {
+        alert(`고객님이 입력하신 아이디는 ${id}이며 비밀번호는 ${password}입니다.`)
+        setId('')
+        setPassword('')
+      }}>
+        로그인</button>
+    </div>
+  )
 }
 
 export default App
