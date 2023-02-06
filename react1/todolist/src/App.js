b691442fcf7e21be378fd11859ed7fb5dfade50a
jaeuk Lee <lskkhj1005@naver.com>
2023-02-06 18:13:42 +0900

    stop todolist

--- react1/todolist/src/App.js
+++ react1/todolist/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  // 초기값과 setTodo 만들기
+  const [todo, setTodo] = useState([
+    { id: 1, title: "리액트 공부하기", contents: "리액트 기초 공부", done: false},
+    { id: 2, title: "리액트 공부합시다", contents: "노션 보기", done: false},
+    { id: 3, title: "리액트 공부하기", contents: "리액트 기초 공부", done: false},
+  ])
+
+  const [title, setTitle] = useState('')
+  const [contents ,setContents] = useState('')
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  }
+
+  const contentsChangeHandler = (event) => {
+    setContents(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>My Todo List</h1>
+      <div>
+        제목
+        <input 
+          value={title}
+          onChange={titleChangeHandler}
         >
-          Learn React
-        </a>
-      </header>
+
+        </input>
+        내용 <input 
+          value={contents}
+          onChange={contentsChangeHandler}
+        >
+        </input>
+        {/* <button onClick={}>추가하기!</button> */}
+      </div>
     </div>
   );
 }
