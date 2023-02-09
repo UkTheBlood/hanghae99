95d7714f464b6e468a7ebca28103827f84171cd9
jaeuk Lee <lskkhj1005@naver.com>
2023-02-09 16:41:44 +0900

    usestate

---
+++ react2/react-week-2/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [number, setNumber] = useState(0);
+
+  return (
+    <>
+      <div>Number State: {number}</div>
+      <button
+        onClick={() => {
+          // 기존 업데이트
+          // 버튼을 클릭했을 때 배치성으로 처리되어 배치 업데이트
+          // 명령을 모아서 같은 것들은 최종적으로 한 번만 실행
+          // setNumber(number + 1);
+          // setNumber(number + 1);
+          // setNumber(number + 1);
+
+          // 렌더링이 잦은건 성능에 이슈가 있는 것
+
+          // 함수형 업데이트
+          // 명령을 모아서 순차적으로 한 번씩 실행 (총 3번이 모임)
+          setNumber((currentNumber) => currentNumber + 1);  // 바뀐 스테이트를 받아옴
+          setNumber((currentNumber) => currentNumber + 1);  // 받아온 스테이트를 다시 바꿈...
+          setNumber((currentNumber) => currentNumber + 1);
+        }}
+      >
+        누르면 UP!
+      </button>
+    </>
+  )
+}
+
+export default App;
