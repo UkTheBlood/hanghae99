a8ef9d75921d7161a7177581adb2fbd2ae0fb70c
jaeuk Lee <lskkhj1005@naver.com>
2023-02-09 20:31:53 +0900

    useRef

--- react2/react-week-2/src/App.jsx
+++ react2/react-week-2/src/App.jsx
@@ -1,12 +1,44 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
 
+  const idRef = useRef('');
+  const pwRef = useRef('');
+
+  const [id, setId] = useState("");
+
+  // 화면이 렌더링 될 때 어떤 작업을 하고싶다. : useEffect!!!!!!!
+  useEffect(() => {
+    idRef.current.focus();
+  }, []);
+
+  // useEffect(() => {
+  //   if (id.length >= 10) {
+  //     pwRef.current.focus();
+  //   }
+  // }, [id])
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        아이디 :
+        <input
+          value={id}
+          onChange={(event) => {
+            setId(event.target.value);
+
+            if (id.length >= 10) {
+              pwRef.current.focus();
+            }
+          }}
+          type="text"
+          ref={idRef}
+        />
+      </div>
+      <div>
+        비밀번호 : <input type="password" ref={pwRef} />
+      </div>
+    </>
   )
 }
 
---
+++ react2/react-week-2/src/UseRef1.jsx
@@ -0,0 +1,39 @@
+import './App.css';
+import { useRef, useState } from 'react';
+
+const style = {
+  border: '1px solid black',
+  margin: '10px',
+  padding: '10px',
+}
+
+function App() {
+
+  const [count, setCount] = useState(0);
+  
+  const countRef = useRef(0);
+
+  const plusStateCountButtonHandler = () => {
+    setCount(count + 1);
+  }
+
+  const plusRefCountButtonHandler = () => {
+    countRef.current++;
+    console.log(countRef.current);
+  }
+
+  return (
+    <>
+      <div style={style}>
+        state 영역입니다. {count} <br />
+        <button onClick={plusStateCountButtonHandler}>state 증가</button>
+      </div>
+      <div style={style}>
+        ref 영역입니다. {countRef.current} <br />
+        <button onClick={plusRefCountButtonHandler}>ref 증가</button>
+      </div>
+    </>
+  )
+}
+
+export default App;
